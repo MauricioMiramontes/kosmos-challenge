2b126249837e6dd42b9abc935d92962e8c3bac47
Mauricio Miramontes Ramirez <mauriciomiramontes58@aragon.unam.mx>
2021-11-24 17:20:03 -0600

    Pequeño cambio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Form from './components/form.js';
+import Formulario from './components/form.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <Form>
-      </Form>
+      <Formulario/>
     </div>
   );
 }
