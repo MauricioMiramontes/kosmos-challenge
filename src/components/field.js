52ba1847f287fa7cf4e84b1601ecff970c0af066
Mauricio Miramontes Ramirez <mauriciomiramontes58@aragon.unam.mx>
2021-11-24 22:10:28 -0600

    Se cambia componente Field a un Switch

--- src/components/field.js
+++ src/components/field.js
@@ -11,71 +11,68 @@ import {
 } from "reactstrap";
 
 function Field(props) {
-  return (
-    <>
-      {
-        props.type === "text" ?
-          <Col sm="7" md="7" >
-            <FormGroup>
-              <InputGroup>
-                <InputGroupText>
-                  {props.label}
-                </InputGroupText>
-                <Input
-                  placeholder={props.type}
-                  type={props.type}
-                  name={props.label}
-                  onChange={props.handleChange}
-                >
-                </Input>
-              </InputGroup>
+
+  switch (props.type) {
+    case "text":
+      return (
+        <Col sm="7" md="7" >
+          <FormGroup>
+            <InputGroup>
+              <InputGroupText>
+                {props.label}
+              </InputGroupText>
+              <Input
+                placeholder={props.type}
+                type={props.type}
+                name={props.label}
+                onChange={props.handleChange}
+              >
+              </Input>
+            </InputGroup>
+          </FormGroup>
+        </Col >
+      )
+    case "select":
+      return (
+        <Col sm="7" md="7" >
+          <FormGroup>
+            <InputGroup>
+              <InputGroupText>
+                {props.label}
+              </InputGroupText>
+              <Input type="select" name="select" id="exampleSelect">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </Input>
+            </InputGroup>
+          </FormGroup>
+        </Col>
+      )
+    case "radio":
+      return (
+        <Col sm="7" md="7" >
+          <FormGroup tag="fieldset">
+            <FormGroup check>
+              <Label check>
+                <Input type="radio" name="radio1" />{' '}
+                Opcion 1
+              </Label>
             </FormGroup>
-          </Col >
-          :
-          <>
-            {props.type === "radio" ?
-              <Col sm="7" md="7" >
-                <FormGroup tag="fieldset">
-                 
-                    <FormGroup check>
-                     
-                      <Label check>
-                        <Input type="radio" name="radio1" />{' '}
-                        Opcion 1
-                      </Label>
-                    </FormGroup>
-                    <FormGroup check>
-                      <Label check>
-                        <Input type="radio" name="radio1" />{' '}
-                        Opcion 2
-                      </Label>
-                    </FormGroup>
-                </FormGroup>
-              </Col >
-              :
-              <>
-                <Col sm="7" md="7" >
-                  <FormGroup>
-                    <InputGroup>
-                      <InputGroupText>
-                        {props.label}
-                      </InputGroupText>
-                      <Input type="select" name="select" id="exampleSelect">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                      </Input>
-                    </InputGroup>
-                  </FormGroup>
-                </Col>
-              </>
-            }
-          </>
-      }
-    </>
-  );
+            <FormGroup check>
+              <Label check>
+                <Input type="radio" name="radio1" />{' '}
+                Opcion 2
+              </Label>
+            </FormGroup>
+          </FormGroup>
+        </Col >
+      )
+    default:
+      return (<></>)
+  }
 }
 
 export default Field;
